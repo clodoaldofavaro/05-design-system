62e9e4995f1e9ec95eba9e74d817b7f9b630f2ab
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2024-06-17 02:13:35 -0300

    chore: set eslint config

--- packages/tokens/src/index.ts
+++ packages/tokens/src/index.ts
@@ -1,15 +1,15 @@
-export * from './colors';
+export * from './colors'
 
-export * from './space';
-export * from './radii';
+export * from './space'
+export * from './radii'
 
-export * from './fonts';
-export * from './font-sizes';
-export * from './font-weights';
-export * from './line-heights';
+export * from './fonts'
+export * from './font-sizes'
+export * from './font-weights'
+export * from './line-heights'
 
-//without index.ts
-//import { colors } from '@ignite-ui/tokens/colors
+// without index.ts
+// import { colors } from '@ignite-ui/tokens/colors
 
-//with index.ts
-//import { colors } from '@ignite-ui/tokens/
+// with index.ts
+// import { colors } from '@ignite-ui/tokens/
