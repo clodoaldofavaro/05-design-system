2275f053934ecbdae54f8dca1f2b5fd59d77f96c
Clodoaldo Favaro <clodoaldofavero@gmail.com>
2024-06-17 00:40:44 -0300

    feat: create tokens

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,15 @@
+export * from './colors';
+
+export * from './space';
+export * from './radii';
+
+export * from './fonts';
+export * from './font-sizes';
+export * from './font-weights';
+export * from './line-heights';
+
+//without index.ts
+//import { colors } from '@ignite-ui/tokens/colors
+
+//with index.ts
+//import { colors } from '@ignite-ui/tokens/
